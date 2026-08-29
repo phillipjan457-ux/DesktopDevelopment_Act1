3b9be61cd47cf7b43d65fc819b8d3e70350e9396
Phillip <Phillipjan457@gmail.com>
2026-08-30 02:59:19 +0800

    Removed Unnecessary method

--- EquipmentBorrowing/src/EquipmentBorrowing.Application/Interfaces/IBorrowingRepository.cs
+++ EquipmentBorrowing/src/EquipmentBorrowing.Application/Interfaces/IBorrowingRepository.cs
@@ -6,6 +6,5 @@ public interface IBorrowingRepository
 {
     Task<Borrowing?> SaveBorrowing(Borrowing borrowing, CancellationToken cancellationToken = default);
     Task<IEnumerable<Borrowing>> ListOfBorrows(string studentId, CancellationToken cancellationToken = default);
-    Task<Borrowing?> RecordBorrow(string BorrowId, CancellationToken cancellationToken = default);
-    Task BorrowState(string BorrowId, BorrowingStatus Status, CancellationToken cancellationToken = default);
+
 }
